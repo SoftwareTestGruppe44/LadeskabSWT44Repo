d346b137428647fc00bfad92fc744d1f08ef15d7
au636574 <mikkel.thomasen@gmail.com>
2021-03-17 09:35:14 +0100

    added test to DoorTest

---
+++ Ladeskab.Unit.Test/DoorTest.cs
@@ -0,0 +1,31 @@
+﻿using LadeskabClassLibrary.Door;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Test
+{
+    [TestFixture()]
+    public class DoorTest
+    {
+        private Door _door;
+        private DoorEventArgs _receivedEventArgs;
+
+        [SetUp]
+        public void Setup()
+        {
+            _receivedEventArgs = null;
+            _door = new Door();
+            _door.DoorIsOpen = false;
+            _door.StateChanged +=
+                (o, args) =>
+                {
+                    _receivedEventArgs = args;
+                };
+        }
+        [Test]
+        public void UnlockDoor_DoorIsOpenChangesState_EventFired()
+        {
+            _door.UnlockDoor(_door.DoorIsOpen);
+            Assert.That(_receivedEventArgs, Is.Not.Null);
+        }
+    }
+}
