9714303cc53d690d81fda4e1183f73bc1ba07491
RStavnager <33657263+RStavnager@users.noreply.github.com>
2021-03-17 09:37:03 +0100

    Changed console to myconsole

---
+++ Ladeskab.Unit.Test/UserInterfaceTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using LadeskabClassLibrary.Console;
+using LadeskabClassLibrary.UserInterface;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Ladeskab.Unit.Test
+{
+    public class UserInterfaceTest
+    {
+        private IMyConsole _myConsole;
+        private HdDisplay _display;
+        [SetUp]
+        public void Setup()
+        {
+            _myConsole = Substitute.For<IMyConsole>();
+            _display = new HdDisplay(_myConsole);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Arrange
+
+            //Act
+            _display.ConnectPhone();
+            //Assert
+            _myConsole.Received(1).WriteLine("Please connect your phone to the charger and close the door.");
+        }
+    }
+}
