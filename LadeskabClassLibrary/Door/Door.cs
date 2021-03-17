9e1d17fd84fc0a41982a2a2abc5d67883ba4ae2d
au636574 <mikkel.thomasen@gmail.com>
2021-03-17 14:34:43 +0100

    Handling of Lock- and unlockdoor

--- LadeskabClassLibrary/Door/Door.cs
+++ LadeskabClassLibrary/Door/Door.cs
@@ -4,16 +4,20 @@ namespace LadeskabClassLibrary.Door
 {
     public class Door : IDoor
     {
+        // event handler based on DoorEventArgs
         public event EventHandler<DoorEventArgs> StateChanged;
-        public bool DoorIsOpen { get; set; }
-        public bool DoorLocked { get; set; }
+        // Properties
+        public bool DoorIsOpen { get; private set; }
+        public bool DoorLocked { get; private set; }
 
+        // constructor with defautl values of properties
         public Door()
         {
             DoorIsOpen = false;
             DoorLocked = false;
         }
 
+        // Method to open door
         public void DoorOpen()
         {
             if (DoorIsOpen == true) return;
@@ -21,25 +25,26 @@ namespace LadeskabClassLibrary.Door
             DoorIsOpen = true;
             OnStateChanged(new DoorEventArgs() { DoorOpen = DoorIsOpen });
         }
-
+        // Method to close door
         public void DoorClose()
         {
             if (DoorIsOpen == false) return;
             DoorIsOpen = false;
             OnStateChanged(new DoorEventArgs() { DoorOpen = DoorIsOpen });
         }
+        // Method to lock door
         public void LockDoor()
         {
-            if (DoorLocked == true) return;
+            if (DoorLocked == true || DoorIsOpen == true) return;
             DoorLocked = true;
         }
-
+        // Method to unlock door
         public void UnlockDoor()
         {
-            if (DoorLocked == false) return;
+            if (DoorLocked == false || DoorIsOpen == true) return;
             DoorLocked = false;
         }
-
+        // Raise event
         protected virtual void OnStateChanged(DoorEventArgs e)
         {
             StateChanged?.Invoke(this, e);
