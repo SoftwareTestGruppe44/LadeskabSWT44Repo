bbbe901b3f42dd578537058a79a5b55e2db10da2
BjornLarsen1 <64773431+BjornLarsen1@users.noreply.github.com>
2021-03-17 09:57:41 +0100

    nothing new

--- Ladeskab.Unit.Test/ChargeControllerTests/ChargeControlTests.cs
+++ Ladeskab.Unit.Test/ChargeControllerTests/ChargeControlTests.cs
@@ -1,18 +1,52 @@
+using LadeskabClassLibrary.ChargeController;
+using LadeskabClassLibrary.USBCharger;
 using NUnit.Framework;
+using NSubstitute;
 
 namespace Ladeskab.Unit.Test
 {
     public class ChargeControlTests
     {
+        private IChargeControl _chargeControl;
+        private IUsbCharger _usbCharger;
         [SetUp]
         public void Setup()
         {
+            _chargeControl = Substitute.For<IChargeControl>();
+            _usbCharger = Substitute.For<IUsbCharger>();
         }
 
         [Test]
-        public void Test1()
+        public void check_isConnected()
         {
+            //Arrange
+            _chargeControl.isConnected();
+            //Act
+
+            //Assert
             Assert.Pass();
+        }
+
+        [Test]
+        public void check_startCharge()
+        {
+            //Arrange
+
+            //Act
+            _usbCharger.StartCharge();
+            //Assert
+            _usbCharger.Received(1).StartCharge();
+        }
+
+        [Test]
+        public void check_stopCharge()
+        {
+            //Arrange
+
+            //Act
+            _usbCharger.StopCharge();
+            //Assert
+            _usbCharger.Received(1).StopCharge();
         }
     }
 }
