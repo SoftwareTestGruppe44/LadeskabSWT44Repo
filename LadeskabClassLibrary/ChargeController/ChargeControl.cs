6817f7004c138789ee7c9425238427f2eda0c4f8
BjornLarsen1 <64773431+BjornLarsen1@users.noreply.github.com>
2021-03-17 09:18:22 +0100

    Added some charge control

--- LadeskabClassLibrary/ChargeController/ChargeControl.cs
+++ LadeskabClassLibrary/ChargeController/ChargeControl.cs
@@ -1,7 +1,32 @@
-﻿namespace LadeskabClassLibrary.ChargeController
+﻿using System;
+using LadeskabClassLibrary.USBCharger;
+using static LadeskabClassLibrary.USBCharger.IUsbCharger;
+
+namespace LadeskabClassLibrary.ChargeController
 {
-    public class ChargeControl
+    public class ChargeControl : IChargeControl
     {
-        
+        private UsbChargerSimulator usbCharger = new UsbChargerSimulator();
+        public bool isConnected()
+        {
+            if (usbCharger.Connected == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void startCharge()
+        {
+            usbCharger.StartCharge();
+        }
+
+        public void stopCharge()
+        {
+            usbCharger.StopCharge();
+        }
     }
 }
