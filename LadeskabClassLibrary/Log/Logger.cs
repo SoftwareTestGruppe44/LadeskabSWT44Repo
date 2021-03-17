d42167e7b3ceb00162abb95447cf47a9a62b8e29
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-03-17 13:46:29 +0100

    Added implementation of LogFile and Logger.

---
+++ LadeskabClassLibrary/Log/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace LadeskabClassLibrary.Log
+{
+    public class Logger : ILogger
+    {
+        public void WriteToFile(string text)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            File.AppendAllText(Path.Combine(path, "Logger.txt"), text);
+        }
+    }
+}
