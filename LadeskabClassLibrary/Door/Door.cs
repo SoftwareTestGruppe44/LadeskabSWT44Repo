1028793acade4dd8a6863088c27d93a17a2ba0f9
au636574 <mikkel.thomasen@gmail.com>
2021-03-17 14:30:57 +0100

    Door: LockDoor and Unlock now only locks/unlcoks if the door is closed. Added comments.

--- LadeskabClassLibrary/Door/Door.cs
+++ LadeskabClassLibrary/Door/Door.cs
@@ -4,17 +4,20 @@ namespace LadeskabClassLibrary.Door
 {
     public class Door : IDoor
     {
-       
+        // defining event based on based on DoorEventArgs
         public event EventHandler<DoorEventArgs> StateChanged;
-        public bool DoorIsOpen { get; set; }
-        public bool DoorLocked { get; set; }
+        //Properties
+        public bool DoorIsOpen { get; private set; }
+        public bool DoorLocked { get; private set; }
 
+        // Constructor with default values of properties
         public Door()
         {
             DoorIsOpen = false;
             DoorLocked = false;
         }
 
+        // Method to open door
         public void DoorOpen()
         {
             if (DoorIsOpen == true) return;
@@ -23,7 +26,7 @@ namespace LadeskabClassLibrary.Door
             OnStateChanged(new DoorEventArgs() { DoorOpen = DoorIsOpen });
         }
 
-
+        // Method to close door
         public void DoorClose()
         {
             if (DoorIsOpen == false) return;
@@ -31,20 +34,21 @@ namespace LadeskabClassLibrary.Door
             OnStateChanged(new DoorEventArgs() { DoorOpen = DoorIsOpen });
 
         }
+        // Method to lock door
         public void LockDoor()
         {
-            if (DoorLocked == true) return;
+            if (DoorLocked == true || DoorIsOpen == true) return;
             DoorLocked = true;
         }
 
-
+        // Method to unlock door
         public void UnlockDoor()
         {
-            if (DoorLocked == false) return;
+            if (DoorLocked == false || DoorIsOpen == true) return;
             DoorLocked = false;
 
         }
-
+        // raise an event
         protected virtual void OnStateChanged(DoorEventArgs e)
         {
             StateChanged?.Invoke(this, e);
