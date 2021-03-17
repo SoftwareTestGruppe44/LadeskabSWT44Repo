ae256bddd5ecbd3895b5b3aa925e803b07ed5d16
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-03-17 14:09:49 +0100

    Testet LogFile class

--- Ladeskab.Unit.Test/LogTest.cs
+++ Ladeskab.Unit.Test/LogTest.cs
@@ -1,7 +1,39 @@
-﻿namespace Ladeskab.Unit.Test
+﻿using LadeskabClassLibrary.Log;
+using LadeskabClassLibrary.MyConsole;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskab.Unit.Test
 {
     public class LogTest
     {
-        
+        private ILog _logger;
+        private ILogger _fakeLogger;
+        [SetUp]
+        public void Setup()
+        {
+            _fakeLogger = Substitute.For<ILogger>();
+            _logger = new LogFile(_fakeLogger);
+        }
+
+        [Test]
+        public void LogDoorLocked_ReceivesAndCreateString_CallFakeLogger()
+        {
+            //Arrange
+            _logger.LogDoorLocked(1);
+            //Act
+            //Assert
+            _fakeLogger.ReceivedWithAnyArgs(1).WriteToFile(default);
+        }
+
+        [Test]
+        public void LogDoorUnLocked_ReceivesAndCreateString_CallFakeLogger()
+        {
+            //Arrange
+            _logger.LogDoorUnlocked(1);
+            //Act
+            //Assert
+            _fakeLogger.ReceivedWithAnyArgs(1).WriteToFile(default);
+        }
     }
 }
