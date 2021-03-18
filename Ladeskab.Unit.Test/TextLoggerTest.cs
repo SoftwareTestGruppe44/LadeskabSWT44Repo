87a298cf9b97f3ff5397d6e4b3b3aa661239a48d
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-03-18 15:03:38 +0100

    Added test of TextLogger

--- Ladeskab.Unit.Test/TextLoggerTest.cs
+++ Ladeskab.Unit.Test/TextLoggerTest.cs
@@ -1,20 +1,32 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using System.Linq;
+using LadeskabClassLibrary.Log;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Ladeskab.Unit.Test
 {
     public class TextLoggerTest
     {
+        ILogger _logger = new TextLogger();
+        private ILog _fakeLog;
         [SetUp]
         public void Setup()
         {
+            _fakeLog = Substitute.For<ILog>();
         }
 
         [Test]
-        public void LogDoorLocked_ReceivesAndCreateString_CallFakeLogger()
+        public void WriteToFile_WriteTestOnLastLineInFile_LastLineContainsTest()
         {
             //Arrange
             //Act
+            _logger.WriteToFile("test\n");
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Logger.txt";
+            
             //Assert
+            Assert.That(File.ReadLines(path).Last(), Is.EqualTo("test") );
         }
     }
 }
