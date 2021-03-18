9c7f14d44abd5aab10316cd961cf482dea42412c
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-03-18 14:22:43 +0100

    Added testclass TextLoggerTest and more tests to RfidTest

---
+++ Ladeskab.Unit.Test/TextLoggerTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Ladeskab.Unit.Test
+{
+    public class TextLoggerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void LogDoorLocked_ReceivesAndCreateString_CallFakeLogger()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
